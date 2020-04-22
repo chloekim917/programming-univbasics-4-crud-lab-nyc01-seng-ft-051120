ee5383a6b8ed97f4618153836446c92bbc024c8d
Seoyoung Kim <mintholic1@gmail.com>
2020-04-22 18:04:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array = []
-  new_array << "f"
+  array
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
