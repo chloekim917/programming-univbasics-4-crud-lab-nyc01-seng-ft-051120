a9065705f532d07b70232d86b1470cc0d16bf304
Seoyoung Kim <mintholic1@gmail.com>
2020-04-22 18:05:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,41 +13,34 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  new_array = ['a', 'b', 'c', 'd']
-  new_array.unshift('alpha')
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-   new_array = ['a', 'b', 'c', 'd']
-   new_array.pop()
+   array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-   new_array = ['a', 'b', 'c', 'd']
-   new_array.shift()
+   array.shift()
   
 end
 
 def retrieve_element_from_index(array, index_number)
-   new_array = ['a', 'b', 'c', 'd']
-   new_array[index_number]
+   array[index_number]
   
 end
 
 def retrieve_first_element_from_array(array)
-   new_array = ['a', 'b', 'c', 'd']
-   new_array[0]
+  array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
-   new_array = ['a', 'b', 'c', 'd']
-   new_array[-1]
+  array[-1]
   
 end
 
 def update_element_from_index(array, index_number, element)
-   new_array = ['a', 'b', 'c', 'd']
-   new_array[index_number] = "new"
+  array[index_number] = element
 
 end
